670be27a81b53d5988ca786fbb1337ad665d9438
Annastasia Psitos <annastasia.psitos@gmail.com>
2017-04-03 10:42:18 -0600

    First commit

--- vendingMachine.js
+++ vendingMachine.js
@@ -1,7 +1,14 @@
 export default class VendingMachine {
   constructor() {
     // status can be ["idle", "credited", "vending"]
-    this.state = { status: "idle", credits: 0, change: 0, selection: null }
+    this.state = {
+      status: "idle",
+      credits: 0,
+      change: 0,
+      selection: null,
+      qty: 0,
+      msg: null
+    }
   }
 
   reset() {
