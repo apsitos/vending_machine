7f84cce8a3a86bab1416daf3af2894b77b27b7a5
Annastasia Psitos <annastasia.psitos@gmail.com>
2017-04-06 11:29:11 -0600

    Create treats'

--- treats.js
+++ treats.js
@@ -1,13 +1,15 @@
 export default class Treats {
   constructor() {
     this.state = {
-      name: '',
-      price: null,
-      location: {
+      A1: {name: 'rollos', price: 75}
+      A2: {name: 'pretzels', price: 100}
+      A3: {name: 'cookies', price: 125}
+      B1: {name: 'kit kat', price: 75}
+      B2: {name: 'milky way', price: 75}
+      B3: {name: 'snickers', price: 100}
+      C1: {name: 'lays crinkle cut', price: 75}
+      C2: {name: 'granola bar', price: 75}
+      C3: {name: 'bubble gum', price: 25}
+  };
 
-      }
-    }
-  }
-
-  
 }
